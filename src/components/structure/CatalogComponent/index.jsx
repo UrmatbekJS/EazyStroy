4de3fc53d47e61d97fd4690f02f231f9122e9c4a
unknown <sadyk.ury@gmail.com>
2024-06-29 20:34:57 +0600

    ADD:HEADERLOGO

--- src/components/structure/CatalogComponent/index.jsx
+++ src/components/structure/CatalogComponent/index.jsx
@@ -1,58 +1,37 @@
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./CatalogComponent.scss";
-import Slider from "react-slick";
-import ConstructionComponent from "../ConstructionComponent";
 import BuildingToolsComponent from "../BuildingToolsComponent";
+import ConstructionComponent from "../ConstructionComponent";
+import "./CatalogComponent.scss";
 import HouseholdProductsComponent from "../HouseholdProductsComponent";
-import { useState } from "react";
 
 const CatalogComponent = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
-  };
-
-  const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex < 2 ? prevIndex + 1 : prevIndex));
-  };
-
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    // arrows: true, // Add arrows if needed
   };
 
   return (
-    <div className="CatalogComponent" id="catalog">
+    <div className="CatalogComponent">
       <h2 className="CatalogComponent__Title">Наш каталог</h2>
       <div className="CatalogComponent__Search">
         <input type="search" placeholder="Найти товар" />
+        {/* <img width={25} src="/images/icon_search.png" alt="" /> */}
       </div>
       <Slider {...settings}>
         <div>
-          <ConstructionComponent
-            activeIndex={activeIndex}
-            onPrevClick={handlePrev}
-            onNextClick={handleNext}
-          />
+          <ConstructionComponent />
         </div>
         <div>
-          <BuildingToolsComponent
-            activeIndex={activeIndex}
-            onPrevClick={handlePrev}
-            onNextClick={handleNext}
-          />
+          <BuildingToolsComponent />
         </div>
         <div>
-          <HouseholdProductsComponent
-            activeIndex={activeIndex}
-            onPrevClick={handlePrev}
-            onNextClick={handleNext}
-          />
+          <HouseholdProductsComponent />
         </div>
       </Slider>
     </div>
